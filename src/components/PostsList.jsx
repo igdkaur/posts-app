36a8ffdb10cbef3dd3923f62f64f69f17328905d
GDK <igdkaur@gmail.com>
2024-04-05 08:10:46 +0530

    base code for new post component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,18 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css"
 
+
 function PostsList() {
   return (
+    <>
+    <NewPost />
     <ul className={classes.posts}>
+
       <Post author="Kim" body="The book needs revision" />
       <Post author="Leo" body="The index needs correction" />
     </ul>
+    </>
   );
 }
 

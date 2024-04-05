c4bef09c067d99bbba7c306c72bdca36646765b1
GDK <igdkaur@gmail.com>
2024-04-05 12:51:55 +0530

    edge cases

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -20,7 +20,7 @@ function PostsList({ isPosting, onStopPosting }) {
   if (isPosting) {
     modalContent = (
       <Modal onClose={onStopPosting}>
-        <NewPost onCancel={onStopPosting} onAddPost={handleAddPost}/>
+        <NewPost onCancel={onStopPosting} onAddPost={handleAddPost} />
       </Modal>
     );
   }
@@ -28,14 +28,19 @@ function PostsList({ isPosting, onStopPosting }) {
   return (
     <>
       {modalContent}
-      <ul className={classes.posts}>
-        {/* {posts.map((post) => {
-           <Post author={post.author} body={post.body}/> 
-        })} */}
-        {posts.map((post) => (
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
-      </ul>
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }

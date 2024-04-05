3db786f058b5452a11ec6ffb0439c76e41e8a4ed
GDK <igdkaur@gmail.com>
2024-04-05 07:46:29 +0530

    passing dynamic value

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Post from "./components/Post";
 
+
+
 function App() {
   return <Post />
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const arr = ['Kim Jon', 'Poili Mathew']
+
 function Post() {
+  const randomName = Math.random() > 0.5 ? arr[0] : arr[1];
+
   return (
     <div>
-      <p>kim jon</p>
+      <p>{randomName}</p>
       <p>The book needs revision</p>
     </div>
   )

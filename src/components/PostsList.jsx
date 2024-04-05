2eef11c19172267fb7070d7a6372c321b49f45f9
GDK <igdkaur@gmail.com>
2024-04-05 08:37:00 +0530

    lifting the state 3

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,16 +6,20 @@ import { useState } from 'react';
 
 function PostsList() {
   const [enteredBody, setEnteredBody] = useState();
+  const [enteredAuthor, setEnteredAuthor] = useState();
 
   function handleBodyChange(event) {
     setEnteredBody(event.target.value);
   }
+  function handleAuthorChange(event) {
+    setEnteredAuthor(event.target.value);
+  }
 
   return (
     <>
-      <NewPost onBodyChange={handleBodyChange} />
+      <NewPost onBodyChange={handleBodyChange} onAuthorChange={handleAuthorChange}/>
       <ul className={classes.posts}>
-        <Post author="Kim" body={enteredBody} />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Leo" body="The index needs correction" />
       </ul>
     </>
